a075a0521d07e14fd74adec819c495d3899ae538
Florian Reuschel <florian.reuschel@ownbit.net>
2021-01-08 17:54:26 +0100

    Use jest and sass instead of mocha/chai + node-sass

--- test/index.js
+++ test/index.js
@@ -1,20 +1,19 @@
-/* eslint-env mocha */
+/* eslint-env jest */
 const yamlImporter = require('../dist/node-sass-yaml-importer');
 const { isYAMLfile } = yamlImporter
-const sass = require('node-sass');
-const { expect } = require('chai');
+const sass = require('sass');
 const { resolve } = require('path');
 
-const EXPECTATION = 'body {\n  color: #c33; }\n';
+const EXPECTATION = 'body {\n  color: #c33;\n}';
 
 describe('Import type test', function() {
   it('imports strings', function() {
     let result = sass.renderSync({
       file: './test/fixtures/strings/style.scss',
-      importer: yamlImporter
+      importer: yamlImporter,
     });
 
-    expect(result.css.toString()).to.eql(EXPECTATION);
+    expect(result.css.toString()).toBe(EXPECTATION);
   });
 
   it('imports lists', function() {
@@ -23,7 +22,7 @@ describe('Import type test', function() {
       importer: yamlImporter
     });
 
-    expect(result.css.toString()).to.eql(EXPECTATION);
+    expect(result.css.toString()).toBe(EXPECTATION);
   });
 
   it('imports maps', function() {
@@ -32,7 +31,7 @@ describe('Import type test', function() {
       importer: yamlImporter
     });
 
-    expect(result.css.toString()).to.eql(EXPECTATION);
+    expect(result.css.toString()).toBe(EXPECTATION);
   });
 
   it('finds imports via includePaths', function() {
@@ -42,7 +41,7 @@ describe('Import type test', function() {
       importer: yamlImporter
     });
 
-    expect(result.css.toString()).to.eql(EXPECTATION);
+    expect(result.css.toString()).toBe(EXPECTATION);
   });
 
   it('finds imports via multiple includePaths', function() {
@@ -52,7 +51,7 @@ describe('Import type test', function() {
       importer: yamlImporter
     });
 
-    expect(result.css.toString()).to.eql(EXPECTATION);
+    expect(result.css.toString()).toBe(EXPECTATION);
   });
 
   it(`throws when an import doesn't exist`, function() {
@@ -64,7 +63,7 @@ describe('Import type test', function() {
       });
     }
 
-    expect(render).to.throw(
+    expect(render).toThrow(
       'Unable to find "variables.yml" from the following path(s): ' +
       `${resolve(process.cwd(), 'test/fixtures/include-paths')}, ${process.cwd()}, ./test/fixtures/include-paths/foo. ` +
       'Check includePaths.'
@@ -77,7 +76,7 @@ describe('Import type test', function() {
       importer: yamlImporter
     });
 
-    expect(result.css.toString()).to.eql(EXPECTATION);
+    expect(result.css.toString()).toBe(EXPECTATION);
   });
 
   // TODO: Added to verify named exports + CommonJS default export hack (see index.js).
@@ -87,12 +86,12 @@ describe('Import type test', function() {
       importer: yamlImporter
     });
 
-    expect(result.css.toString()).to.eql(EXPECTATION);
+    expect(result.css.toString()).toBe(EXPECTATION);
   });
 
   // TODO: Added to verify named exports + CommonJS default export hack (see index.js).
   it('provides named exports of internal methods', function() {
-    expect(isYAMLfile('import.yml')).to.be.true;
-    expect(isYAMLfile('import.yaml')).to.be.true;
+    expect(isYAMLfile('import.yml')).toBe(true);
+    expect(isYAMLfile('import.yaml')).toBe(true);
   });
 });
