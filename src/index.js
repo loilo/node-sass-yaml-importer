6bc8964d88ca0549c14d82ef66d7c2b1d276ce96
Florian Reuschel <florian.reuschel@ownbit.net>
2021-01-08 17:53:27 +0100

    Apply Prettier formatting

--- src/index.js
+++ src/index.js
@@ -1,72 +1,77 @@
-import isPlainObject from 'is-plain-object';
-import { readFileSync, existsSync } from 'fs';
-import path, { resolve, dirname } from 'path';
-import yaml            from 'js-yaml';
+import isPlainObject from 'is-plain-object'
+import { readFileSync, existsSync } from 'fs'
+import path, { resolve, dirname } from 'path'
+import yaml from 'js-yaml'
 
-export default function(url, prev) {
+export default function (url, prev) {
   if (!isYAMLfile(url)) {
-    return null;
+    return null
   }
 
-  let includePaths = this.options.includePaths ? this.options.includePaths.split(path.delimiter) : [];
-  let paths = []
-    .concat(dirname(prev))
-    .concat(includePaths);
+  let includePaths = this.options.includePaths
+    ? this.options.includePaths.split(path.delimiter)
+    : []
+  let paths = [].concat(dirname(prev)).concat(includePaths)
 
   let file = paths
     .map(path => resolve(path, url))
     .filter(existsSync)
-    .pop();
+    .pop()
 
   if (!file) {
-    return new Error(`Unable to find "${url}" from the following path(s): ${paths.join(', ')}. Check includePaths.`);
+    return new Error(
+      `Unable to find "${url}" from the following path(s): ${paths.join(
+        ', '
+      )}. Check includePaths.`
+    )
   }
 
   try {
     return {
-      contents: transformJSONtoSass(yaml.load(readFileSync(require.resolve(file), 'utf8')))
-    };
-  } catch(e) {
-    return new Error(`node-sass-yaml-importer: Error transforming YAML to SASS. Check if your YAML parses correctly. ${e}`);
+      contents: transformJSONtoSass(
+        yaml.load(readFileSync(require.resolve(file), 'utf8'))
+      )
+    }
+  } catch (e) {
+    return new Error(
+      `node-sass-yaml-importer: Error transforming YAML to SASS. Check if your YAML parses correctly. ${e}`
+    )
   }
-
 }
 
 export function isYAMLfile(url) {
-  return /\.ya?ml$/.test(url);
+  return /\.ya?ml$/.test(url)
 }
 
 export function transformJSONtoSass(json) {
   return Object.keys(json)
     .map(key => `$${key}: ${parseValue(json[key])};`)
-    .join('\n');
+    .join('\n')
 }
 
 export function parseValue(value) {
   if (Array.isArray(value)) {
-    return parseList(value);
+    return parseList(value)
   } else if (isPlainObject(value)) {
-    return parseMap(value);
+    return parseMap(value)
   } else {
-    return value;
+    return value
   }
 }
 
 export function parseList(list) {
-  return `(${list
-    .map(value => `${parseValue(value)},`)
-    .join('')})`;
+  return `(${list.map(value => `${parseValue(value)},`).join('')})`
 }
 
 export function parseMap(map) {
   return `(${Object.keys(map)
     .map(key => `${key}: ${parseValue(map[key])},`)
-    .join('')})`;
+    .join('')})`
 }
 
 // Super-hacky: Override Babel's transpiled export to provide both
 // a default CommonJS export and named exports.
 // Fixes: https://github.com/Updater/node-sass-json-importer/issues/32
 // TODO: Remove in 3.0.0. Upgrade to Babel6.
-module.exports = exports.default;
-Object.keys(exports).forEach(key => module.exports[key] = exports[key]);
+module.exports = exports.default
+Object.keys(exports).forEach(key => (module.exports[key] = exports[key]))
--- test/index.js
+++ test/index.js
@@ -1,97 +1,103 @@
 /* eslint-env jest */
-const yamlImporter = require('../dist/node-sass-yaml-importer');
+const yamlImporter = require('../dist/node-sass-yaml-importer')
 const { isYAMLfile } = yamlImporter
-const sass = require('sass');
-const { resolve } = require('path');
+const sass = require('sass')
+const { resolve } = require('path')
 
-const EXPECTATION = 'body {\n  color: #c33;\n}';
+const EXPECTATION = 'body {\n  color: #c33;\n}'
 
-describe('Import type test', function() {
-  it('imports strings', function() {
+describe('Import type test', function () {
+  it('imports strings', function () {
     let result = sass.renderSync({
       file: './test/fixtures/strings/style.scss',
-      importer: yamlImporter,
-    });
+      importer: yamlImporter
+    })
 
-    expect(result.css.toString()).toBe(EXPECTATION);
-  });
+    expect(result.css.toString()).toBe(EXPECTATION)
+  })
 
-  it('imports lists', function() {
+  it('imports lists', function () {
     let result = sass.renderSync({
       file: './test/fixtures/lists/style.scss',
       importer: yamlImporter
-    });
+    })
 
-    expect(result.css.toString()).toBe(EXPECTATION);
-  });
+    expect(result.css.toString()).toBe(EXPECTATION)
+  })
 
-  it('imports maps', function() {
+  it('imports maps', function () {
     let result = sass.renderSync({
       file: './test/fixtures/maps/style.scss',
       importer: yamlImporter
-    });
+    })
 
-    expect(result.css.toString()).toBe(EXPECTATION);
-  });
+    expect(result.css.toString()).toBe(EXPECTATION)
+  })
 
-  it('finds imports via includePaths', function() {
+  it('finds imports via includePaths', function () {
     let result = sass.renderSync({
       file: './test/fixtures/include-paths/style.scss',
       includePaths: ['./test/fixtures/include-paths/variables'],
       importer: yamlImporter
-    });
+    })
 
-    expect(result.css.toString()).toBe(EXPECTATION);
-  });
+    expect(result.css.toString()).toBe(EXPECTATION)
+  })
 
-  it('finds imports via multiple includePaths', function() {
+  it('finds imports via multiple includePaths', function () {
     let result = sass.renderSync({
       file: './test/fixtures/include-paths/style.scss',
-      includePaths: ['./test/fixtures/include-paths/variables', './some/other/path/'],
+      includePaths: [
+        './test/fixtures/include-paths/variables',
+        './some/other/path/'
+      ],
       importer: yamlImporter
-    });
+    })
 
-    expect(result.css.toString()).toBe(EXPECTATION);
-  });
+    expect(result.css.toString()).toBe(EXPECTATION)
+  })
 
-  it(`throws when an import doesn't exist`, function() {
+  it(`throws when an import doesn't exist`, function () {
     function render() {
       sass.renderSync({
         file: './test/fixtures/include-paths/style.scss',
         includePaths: ['./test/fixtures/include-paths/foo'],
         importer: yamlImporter
-      });
+      })
     }
 
     expect(render).toThrow(
       'Unable to find "variables.yml" from the following path(s): ' +
-      `${resolve(process.cwd(), 'test/fixtures/include-paths')}, ${process.cwd()}, ./test/fixtures/include-paths/foo. ` +
-      'Check includePaths.'
-    );
-  });
+        `${resolve(
+          process.cwd(),
+          'test/fixtures/include-paths'
+        )}, ${process.cwd()}, ./test/fixtures/include-paths/foo. ` +
+        'Check includePaths.'
+    )
+  })
 
-  it('ignores non-json imports', function() {
+  it('ignores non-json imports', function () {
     let result = sass.renderSync({
       file: './test/fixtures/non-json/style.scss',
       importer: yamlImporter
-    });
+    })
 
-    expect(result.css.toString()).toBe(EXPECTATION);
-  });
+    expect(result.css.toString()).toBe(EXPECTATION)
+  })
 
   // TODO: Added to verify named exports + CommonJS default export hack (see index.js).
-  it('provides the default export when using node require to import', function() {
+  it('provides the default export when using node require to import', function () {
     let result = sass.renderSync({
       file: './test/fixtures/strings/style.scss',
       importer: yamlImporter
-    });
+    })
 
-    expect(result.css.toString()).toBe(EXPECTATION);
-  });
+    expect(result.css.toString()).toBe(EXPECTATION)
+  })
 
   // TODO: Added to verify named exports + CommonJS default export hack (see index.js).
-  it('provides named exports of internal methods', function() {
-    expect(isYAMLfile('import.yml')).toBe(true);
-    expect(isYAMLfile('import.yaml')).toBe(true);
-  });
-});
+  it('provides named exports of internal methods', function () {
+    expect(isYAMLfile('import.yml')).toBe(true)
+    expect(isYAMLfile('import.yaml')).toBe(true)
+  })
+})
